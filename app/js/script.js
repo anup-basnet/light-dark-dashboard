8018eb92dda97a51089a664ad889984fd44db8df
Anup <anupbasnet057@gmail.com>
2023-05-10 15:00:48 +0545

    feat: toggle with localStorage for user prefernce
    Toggle is functioning with localStorage storing the user preferred theme to toggle between light / dark mode.

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,56 @@
-console.log('Helllo, there')
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+
+
+const setColorMode = () => {
+    if(localStorage.getItem('colorMode') == 'dark') {
+        setDarkMode(); 
+        darkButton.click();
+    } else if(localStorage.getItem('colorMode') == 'light'){
+        setLightMode();
+        lightButton.click();
+    }
+}
+
+const checkMode = () => {
+    if(localStorage.getItem('colorMode') == null) {
+        if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+            lightButton.click();
+        } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            darkButton.click();
+        }
+    }
+
+}
+
+const checkModeChange = () => {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+        checkMode();
+    })
+}
+
+const setDarkMode = () => {
+    document.querySelector('body').classList = 'dark'
+}
+
+const setLightMode = () => {
+    document.querySelector('body').classList = 'light'
+}
+
+setColorMode();
+checkMode();
+checkModeChange();
+
+const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+radioButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        
+        if(darkButton.checked) {
+            setDarkMode();
+            localStorage.setItem('colorMode', 'dark')
+        } else {
+            setLightMode();
+            localStorage.setItem('colorMode', 'light')
+        }
+    })
+})
